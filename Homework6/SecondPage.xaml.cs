b02cab402b16953806bdae030198be7c8f99c3b6
jjbman52 <jjbman52@gmail.com>
2019-09-30 10:02:53 -0500

    Initial check-in of module Homework6

---
+++ Homework6/SecondPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Homework6
+{
+    public partial class SecondPage : ContentPage
+    {
+        public SecondPage()
+        {
+            InitializeComponent();
+        }
+        private async void NavigateButton_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+
+        private async void NavigateButton_OnClickedPage3(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ThirdPage());
+        }
+    }
+}
---
+++ Homework6/ThirdPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Homework6
+{
+    public partial class ThirdPage : ContentPage
+    {
+        public ThirdPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void NavigateButton_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SecondPage());
+        }
+    }
+}
